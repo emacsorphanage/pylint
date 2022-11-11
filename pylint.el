c33959dc57af4ca46ea74ca6a07b19f19556c045
Jonas Bernoulli <jonas@bernoul.li>
2022-11-11 22:50:48 +0100

    Refresh permission statement in pylint.el

--- pylint.el
+++ pylint.el
@@ -1,26 +1,27 @@
 ;;; pylint.el --- minor mode for running `pylint'
 
 ;; Copyright (c) 2009, 2010 Ian Eure <ian.eure@gmail.com>
+
 ;; Author: Ian Eure <ian.eure@gmail.com>
 ;; Maintainer: Jonathan Kotta <jpkotta@gmail.com>
 
 ;; Keywords: languages python
 ;; Version: 1.02
 
-;; pylint.el is free software; you can redistribute it and/or modify it
-;; under the terms of the GNU General Public License as published by the Free
-;; Software Foundation; either version 2, or (at your option) any later
-;; version.
+;; SPDX-License-Identifier: GPL-2.0-or-later
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 2 of the License,
+;; or (at your option) any later version.
 ;;
-;; It is distributed in the hope that it will be useful, but WITHOUT ANY
-;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-;; FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-;; details.
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 ;;
-;; You should have received a copy of the GNU General Public License along
-;; with your copy of Emacs; see the file COPYING.  If not, write to the Free
-;; Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-;; MA 02110-1301, USA
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
